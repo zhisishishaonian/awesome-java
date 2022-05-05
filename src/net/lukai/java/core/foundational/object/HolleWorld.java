58e7f44313e9606b60591f053f0711b82eae3db4
zhisishishaonian <2251584713@qq.com>
2022-05-06 01:39:29 +0800

    111

---
+++ src/net/lukai/java/core/foundational/object/HolleWorld.java
@@ -0,0 +1,15 @@
+package net.lukai.java.core.foundational.object;
+
+public class HolleWorld {
+    public static void main(String[] args) {
+        System.out.println("HolleWorld");
+
+    }
+
+
+
+
+
+
+
+}
